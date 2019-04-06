cc59b937535abdab58ffdd54f5a4905068e1aa57
Yecson <49099990+Yecson@users.noreply.github.com>
2019-04-06 15:22:18 -0600

    Add files via upload

---
+++ semana1/Practica-2.py
@@ -0,0 +1,15 @@
+#Esta es la practica para escribir 2 lineas
+#en consola
+
+"""
+comentario en multiples lineas
+util para explicar mas detalles
+"""
+
+print("Hola a todos")
+print("buenos días")
+
+#Varias lineas en un solo print
+print("mi primera linea\nmi segunda linea")
+
+print("hola de nuevo")
